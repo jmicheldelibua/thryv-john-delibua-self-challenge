a925d9d73366848f1f363860227f10f083a88c81
John Michel Delibua <jmicheldelibua@gmail.com>
2025-01-21 18:05:24 -0400

    [UPDATE] adjust ESLint configuration to use ECMAScript 2021 and modify rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 2021, // Updated to match `tsconfig.json` target
       sourceType: 'module',
       parserOptions: {
         projectService: true,
@@ -29,7 +29,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': 0,
     },
   },
 );
