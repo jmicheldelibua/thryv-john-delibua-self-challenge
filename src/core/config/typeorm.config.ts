b4bef7e60f27991bc79e739313739706d204b039
John Michel Delibua <jmicheldelibua@gmail.com>
2025-01-21 18:34:32 -0400

    [UPDATE] modify database configuration to use environment variables and update start scripts to include .env file

--- src/core/config/typeorm.config.ts
+++ src/core/config/typeorm.config.ts
@@ -2,12 +2,12 @@ import { registerAs } from "@nestjs/config";
 import { TYPE_ORM_CONFIG_TOKEN } from "@core/constant";
 
 const config = {
-    type: 'mysql',
-    host: process.env.DB_HOST || 'localhost',
-    port: (process.env.DB_PORT || 3306) as number,
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'taskmanager',
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT as unknown as number,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [],
     synchronize: process.env.NODE_ENV !== 'production',
 }
