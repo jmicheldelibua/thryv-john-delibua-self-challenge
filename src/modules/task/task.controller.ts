e74f71a7938737db731d080bbe16c067fa863510
jmicheldelibua <jmicheldelibua@gmail.com>
2025-01-22 13:16:52 -0400

    feat: Add file upload functionality to task controller

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -6,14 +6,16 @@ import {
   Patch,
   Param,
   Delete,
+  UploadedFile,
 } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
 @Controller('task')
 export class TaskController {
-  constructor(private readonly taskService: TaskService) {}
+  constructor(private readonly taskService: TaskService) { }
 
   @Post()
   create(@Body() createTaskDto: CreateTaskDto) {
@@ -34,6 +36,27 @@ export class TaskController {
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.taskService.update(+id, updateTaskDto);
   }
+
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Add files to a task (NO IMPLEMENTATION YET)',
+    schema: {
+      type: 'object',
+      properties: {
+        taskFiles: {
+          type: 'string',
+          format: 'binary',
+          description: 'Tasks Files (opcional)',
+        },
+      },
+    },
+  })
+  @Post('file/:id')
+  addFile(@Param('id') id: string, @UploadedFile() taskFiles?: Express.Multer.File[]) {
+    return `Pending to upload ${taskFiles?.length} files to AWS S3`;
+  }
+
+
 
   @Delete(':id')
   remove(@Param('id') id: string) {
