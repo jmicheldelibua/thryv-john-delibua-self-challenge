c45772240907e72cf4749170f4db40c8c6e2b71b
John Michel Delibua <jmicheldelibua@gmail.com>
2025-01-21 20:06:12 -0400

    [ADD] integrate Swagger for API documentation and update package dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription('Task Manager API endpoint for managing tasks')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
