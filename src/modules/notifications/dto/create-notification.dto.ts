04140e7dcd5db3d778522886f6b1a5043606f192
jmicheldelibua <jmicheldelibua@gmail.com>
2025-01-23 18:19:18 -0400

    feat: Add API endpoint for creating notifications

--- src/modules/notifications/dto/create-notification.dto.ts
+++ src/modules/notifications/dto/create-notification.dto.ts
@@ -1,11 +1,27 @@
-import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsNumberString, IsOptional, IsString } from "class-validator";
 
 export class CreateNotificationDto {
+    @ApiProperty({ example: '5f8f6b2b-7b8d-4b7d-8b3d-3b7d8b3d7b8d', description: 'Notification id' })
     @IsString()
     notificationId: string;
+
+    @ApiProperty({ example: 1, description: 'UserId to be send the notification' })
+    @IsNumberString()
     sentTo: number;
+
+    @ApiProperty({ example: 'New Notification', description: 'Notification title' })
+    @IsString()
     title: string;
-    date: Date;
+
+    @ApiProperty({ example: 'This is a new notification', description: 'Notification body' })
+    @IsString()
     body: string;
+
+    @ApiProperty({ example: true, description: 'Notification seen status' })
+    @IsBoolean()
+    @IsOptional()
     seen: boolean
+
+    date: Date;
 }
--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Body, Post, Patch } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { Public } from '@core/decorators';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { UpdateNotificationDto } from './dto/update-notification.dto';
 
 @ApiTags('notifications')
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) { }
 
-  @Public()
+
+  @ApiProperty({ description: 'Create a new notification' })
+  @Post()
+  create(@Body() createNotificationDto: CreateNotificationDto) {
+    return this.notificationsService.create(createNotificationDto);
+  }
+
+
   @ApiProperty({ description: 'Get all notifications' })
   @Get()
   findAll() {
@@ -19,6 +28,11 @@ export class NotificationsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.notificationsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTaskDto: UpdateNotificationDto) {
+    return this.notificationsService.update(id, updateTaskDto);
   }
 
   @ApiProperty({ description: 'Delete a notification by id' })
